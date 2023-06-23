6c30a8db003182a3312dfe0b022604efb88b9a47
Andisurya01 <90569235+Andisurya01@users.noreply.github.com>
2023-06-24 00:14:39 +0700

    Update IgInfor.jsx

--- src/AllComponentsHere/Components Home/IgInfor.jsx
+++ src/AllComponentsHere/Components Home/IgInfor.jsx
@@ -40,7 +40,7 @@ const Postingan = () => {
             </div>
             <div className='m-5 h-96  bg-cover rounded-2xl overflow-auto max-w-screen-xl mx-auto' >
                 <div className='w-full h-full rounded-2xl justify-center scrollbar overflow-y-scroll'>
-                    <img className='mx-auto content-center' src='../Postinganig.png' />
+                    <img className='mx-auto content-center' src='./Postinganig.png' />
                 </div>
             </div>
             <div>
