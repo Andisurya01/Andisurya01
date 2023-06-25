884e462ece01c9d5e2438c0eefe045a2997c57db
Andisurya01 <90569235+Andisurya01@users.noreply.github.com>
2023-06-25 20:29:08 +0700

    Update ProjectKuli2.jsx

--- src/AllComponentsHere/Components Home/ProjectKuli2.jsx
+++ src/AllComponentsHere/Components Home/ProjectKuli2.jsx
@@ -1,12 +1,11 @@
 const ProjectKuli2 = () => {
     return (
         <div>
-            <div className="max-w-screen-xl p-5 wt-20 rounded-2xl bg-bgresto bg-center mx-auto h-96 overflow-auto">
+            <div className="max-w-screen-xl pt-10 rounded-2xl bg-bgresto bg-center mx-auto h-96 overflow-auto">
                 <div className="container p-16 text-neutral-200">
                     <p className="text-sm mb-5">Tugas Kuliah Semester 2</p>
                     <h1 className="font-bebas text-4xl">Aplikasi Crud, <a className="">Menu Restaurant</a></h1>
                     <p className="text-sm mt-5">Mengaplikasikan CRUD menggunakan php, </p>
-
                     <button className="bg-neutral-800 hover:bg-stone-400 hover:text-stone-950 duration-200 text-sm rounded-md mt-10 h-10 w-52">VIEW CASE STUDY</button>
                 </div>
             </div>
