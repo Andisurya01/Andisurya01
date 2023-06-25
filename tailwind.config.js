9b63dc4f491dc6729ee792078092086e2aedc761
Andisurya01 <90569235+Andisurya01@users.noreply.github.com>
2023-06-25 20:11:42 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,jsx}", "./dist/**/*.{html,jsx}","./node_modules/flowbite/**/*.js"],
+  content: ["./src/**/*.{html,jsx}", "./dist/**/*.{html,jsx}","./node_modules/flowbite/**/*.js",'./pages/**/*.{html,js}',
+    './components/**/*.{html,js}'],
   darkMode : 'class',
   theme: {
     extend: {
