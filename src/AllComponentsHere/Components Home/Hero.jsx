2361e77656e28203dd5687702b38402ef0992dbc
Andisurya01 <90569235+Andisurya01@users.noreply.github.com>
2023-06-25 20:45:20 +0700

    Update Hero.jsx

--- src/AllComponentsHere/Components Home/Hero.jsx
+++ src/AllComponentsHere/Components Home/Hero.jsx
@@ -1,7 +1,7 @@
 const Hero = () => {
 
     return (
-        <div className=" max-w-screen-xl mx-auto grid grid-cols-1 p-5 md:grid-cols-2 mt-60 p-5">
+        <div className=" max-w-screen-xl mx-auto grid grid-cols-1 p-5 md:grid-cols-2 mt-60 p-10">
             <div className="">
                 <h1 className="font-bebas text-8xl text-stone-950">Hai!,My Name Is Andi, people who like visuals</h1>
                 <hr className="my-6 sm:mx-auto border-stone-700 lg:my-8 mt-20" /><div></div>
