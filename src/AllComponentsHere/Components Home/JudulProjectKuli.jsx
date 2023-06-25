a3f5b9c52c584738025a9fb17bf6935663efa549
Andisurya01 <90569235+Andisurya01@users.noreply.github.com>
2023-06-25 20:14:38 +0700

    Update JudulProjectKuli.jsx

--- src/AllComponentsHere/Components Home/JudulProjectKuli.jsx
+++ src/AllComponentsHere/Components Home/JudulProjectKuli.jsx
@@ -1,7 +1,7 @@
 const JudulProject = () => {
     return (
         <section className="max-w-screen-xl font-bebas mx-auto text-7xl text-start  mb-20">
-            <h1>project Kampus</h1>
+            <h1>Campus project</h1>
             
         </section>
     )
