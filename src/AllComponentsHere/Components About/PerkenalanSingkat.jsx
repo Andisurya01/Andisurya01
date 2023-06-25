c18fb1d49abb0d68407a42b9475a75cf344d0229
Andisurya01 <90569235+Andisurya01@users.noreply.github.com>
2023-06-25 20:18:25 +0700

    Update PerkenalanSingkat.jsx

--- src/AllComponentsHere/Components About/PerkenalanSingkat.jsx
+++ src/AllComponentsHere/Components About/PerkenalanSingkat.jsx
@@ -2,7 +2,7 @@ const PerkenalanSingkat = () => {
 
     return(
         <div className="bg-neutral-700 w-full h-96">
-            <h1 className="text-stone-300 font-bebas text-4xl text-center pt-44 ">Gua adalah mahasiswa Informatika yang cinta<br/> terhadap hal baru dan menaklukannya</h1>
+            <h1 className="text-stone-300 font-bebas text-4xl text-center pt-44 ">I am an Informatics student<br/> new things and conquering them</h1>
         </div>
     )
 }
