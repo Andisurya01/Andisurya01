77c6adf7b39842a49565d02301db11175019c285
Andisurya01 <90569235+Andisurya01@users.noreply.github.com>
2023-06-25 20:35:47 +0700

    Update ProjectKuli2.jsx

--- src/AllComponentsHere/Components Home/ProjectKuli2.jsx
+++ src/AllComponentsHere/Components Home/ProjectKuli2.jsx
@@ -1,6 +1,5 @@
 const ProjectKuli2 = () => {
     return (
-        <div>
             <div className="max-w-screen-xl pt-10 rounded-2xl bg-bgresto bg-center mx-auto h-96 overflow-auto">
                 <div className="container p-16 text-neutral-200">
                     <p className="text-sm mb-5">Tugas Kuliah Semester 2</p>
@@ -9,7 +8,6 @@ const ProjectKuli2 = () => {
                     <button className="bg-neutral-800 hover:bg-stone-400 hover:text-stone-950 duration-200 text-sm rounded-md mt-10 h-10 w-52">VIEW CASE STUDY</button>
                 </div>
             </div>
-        </div>
     )
 }
 
