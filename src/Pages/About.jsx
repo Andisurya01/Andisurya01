480d0b31783a47fd9bfc974fd821b098e7eb12eb
Andisurya01 <90569235+Andisurya01@users.noreply.github.com>
2023-07-10 10:34:43 +0700

    Update About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,6 +3,8 @@ import Navbar from "../AllComponentsHere/Components Home/navbar";
 import Footerandi from "../AllComponentsHere/Components Home/Footerandi";
 import PerkenalanSingkat from "../AllComponentsHere/Components About/PerkenalanSingkat"
 import PerkenalanPanjang from "../AllComponentsHere/Components About/PerkenalanPanjang";
+import HR from "../AllComponentsHere/Components Abstract/GarisHorizontal";
+import Serti from "../AllComponentsHere/Components About/Sertifikat";
 import Playlist from "../AllComponentsHere/Components About/PLaylist";
 
 const About = () => {
@@ -11,6 +13,8 @@ const About = () => {
             <Navbar></Navbar>
             <PerkenalanSingkat></PerkenalanSingkat>
             <PerkenalanPanjang></PerkenalanPanjang>
+            <HR/>
+            <Serti/>
             <Footerandi></Footerandi>
         </div>
     )
