5197394f86e41e78bd731304e7ce4f34293e3231
Andisurya01 <90569235+Andisurya01@users.noreply.github.com>
2023-07-10 10:33:11 +0700

    Update Sertifikat.jsx

---
+++ src/AllComponentsHere/Components About/Sertifikat.jsx
@@ -0,0 +1,12 @@
+const Serti = () => {
+    return (
+        <div className="max-w-screen-xl mx-auto">
+            <h1 className="text-4xl font-bebas mb-10">
+                My Portofolio
+            </h1>
+            <a className="bg-neutral-900 text-white p-5 mt-10" href="https://drive.google.com/drive/folders/1J42rIpUHHqV-aN7Ww3u2HJudViUH_XnV?usp=sharing">link google drive list my certificate</a>
+        </div>
+    )
+}
+
+export default Serti
