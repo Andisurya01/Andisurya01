b1fca72aa9fd4911a4970c1d633b64d48f080d57
Andisurya01 <andi.suryapriyadi@gmail.com>
2023-09-15 16:02:03 +0700

    Menambahkan mofule flowbite

--- src/AllComponentsHere/Components Home/Hero.jsx
+++ src/AllComponentsHere/Components Home/Hero.jsx
@@ -1,7 +1,7 @@
 const Hero = () => {
 
     return (
-        <div className=" max-w-screen-xl mx-auto grid grid-cols-1 p-5 md:grid-cols-2 mt-60 p-10 mx-10">
+        <div className="max-w-screen-xl mx-auto grid grid-cols-1 md:grid-cols-2 mt-60 p-10">
             <div className="">
                 <h1 className="font-bebas text-8xl text-stone-950">Hai!,My Name Is Andi, people who like visuals</h1>
                 <hr className="my-6 sm:mx-auto border-stone-700 lg:my-8 mt-20" /><div></div>
@@ -9,7 +9,7 @@ const Hero = () => {
                 so I took front-end front-end deh bribe a little bit connected with the major that I'm currently taking</p>
             </div>
             <div className="mx-auto">
-                <img className="border-none w-80" src="https://placeholder.com/300x500"/>
+                <img className="border-none w-80 " src="assets/me.jpeg"/>
             </div>
         </div>
     )
